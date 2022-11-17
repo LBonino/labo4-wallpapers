b81504139765b3d79c9d3f5b4d77c1f888f7bc98
Lisandro Bonino <lisandrobonino4@protonmail.com>
2022-11-17 09:37:39 -0300

    WIP

---
+++ Wallpapers/Controllers/UploadController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Wallpapers.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult()
+        {
+
+        }
+    }
+}
